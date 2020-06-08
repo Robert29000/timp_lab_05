d3d7760c5331c1f4e0a7db2fb16de2f141f942c7
Robert29000 <mayka29000@mail.ru>
2020-06-08 15:38:28 +0300

    fix for homework

---
+++ _builds/clang-cxx14/banking/CMakeFiles/banking.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/melikyan/Desktop/Университет/2 семестр/ТИМП/lab05-master/banking/Account.cpp" "/Users/melikyan/Desktop/Университет/2 семестр/ТИМП/lab05-master/_builds/clang-cxx14/banking/CMakeFiles/banking.dir/Account.cpp.o"
+  "/Users/melikyan/Desktop/Университет/2 семестр/ТИМП/lab05-master/banking/Transaction.cpp" "/Users/melikyan/Desktop/Университет/2 семестр/ТИМП/lab05-master/_builds/clang-cxx14/banking/CMakeFiles/banking.dir/Transaction.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../banking"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
